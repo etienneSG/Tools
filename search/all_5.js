dae8c08d62418304e6c8722e552e9d3c663b9db5
Etienne de Saint Germain <etiennegaillard@gmail.com>
2017-01-18 09:32:59 +0100

    time_tools: get_human_readable_time function

--- search/all_5.js
+++ search/all_5.js
@@ -3,6 +3,7 @@ var searchData=
   ['get_5fatol',['get_atol',['../class_tolerance.html#aaf05647c6bbde0e3ec9659fd5a02b14d',1,'Tolerance']]],
   ['get_5fchosen_5fobjects',['get_chosen_objects',['../class_knapsack.html#acd80014349fbbbd0ca54554aff6d5ecd',1,'Knapsack']]],
   ['get_5fcpu_5ftime',['get_cpu_time',['../time__tools_8h.html#acdbcf9be8c879dddacc5aed7d31d7c84',1,'time_tools.h']]],
+  ['get_5fhuman_5freadable_5ftime',['get_human_readable_time',['../time__tools_8h.html#a163c1f2d2588d962ac38ea3b878298ab',1,'time_tools.h']]],
   ['get_5foptimal_5fvalue',['get_optimal_value',['../class_knapsack.html#a8d80a550c6f5043812ee919457d71458',1,'Knapsack']]],
   ['get_5frtol',['get_rtol',['../class_tolerance.html#a14c102edd2d52808c039bdff6ed09bc9',1,'Tolerance']]],
   ['get_5fwall_5ftime',['get_wall_time',['../time__tools_8h.html#ae9530b09b27f0c6e5c8836ecbfad493f',1,'time_tools.h']]]
--- time__tools_8h.js
+++ time__tools_8h.js
@@ -1,5 +1,6 @@
 var time__tools_8h =
 [
     [ "get_cpu_time", "time__tools_8h.html#acdbcf9be8c879dddacc5aed7d31d7c84", null ],
+    [ "get_human_readable_time", "time__tools_8h.html#a163c1f2d2588d962ac38ea3b878298ab", null ],
     [ "get_wall_time", "time__tools_8h.html#ae9530b09b27f0c6e5c8836ecbfad493f", null ]
 ];
