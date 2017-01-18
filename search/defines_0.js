1d28b6beafb5c0e888e4f1a4fda71b36911c86eb
Etienne de Saint Germain <etiennegaillard@gmail.com>
2017-01-18 09:36:49 +0100

    time_tools: get_human_readable_time() function

--- search/defines_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['array2d_5fdot_5fproduct',['ARRAY2D_DOT_PRODUCT',['../array2d_8h.html#a58c9aa636c5c585f91960cdd427cb5af',1,'array2d.h']]]
-];
--- search/defines_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['print_5farray2d',['PRINT_ARRAY2D',['../array2d_8h.html#a5fc646ca5e64169693d46833630f09ab',1,'array2d.h']]]
-];
