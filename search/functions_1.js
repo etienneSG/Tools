0b7629a102784ca7d1fbacd332d2ad2c38a5f57e
Etienne de Saint Germain <etiennedesaintgermain@gmail.com>
2017-09-04 14:16:26 +0200

    Documentation of copy function in Array2d

--- search/functions_1.js
+++ search/functions_1.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['clear',['clear',['../class_array2d.html#a0d71b4321f0ff406755a1762d6f33b42',1,'Array2d']]],
-  ['close',['close',['../class_tolerance.html#adf5c1af5276a09804b2a975b9998ef58',1,'Tolerance']]]
+  ['close',['close',['../class_tolerance.html#adf5c1af5276a09804b2a975b9998ef58',1,'Tolerance']]],
+  ['copy',['copy',['../class_array2d.html#a7f6d0a1be8c1a2e9a65a1a2951f3b23f',1,'Array2d']]]
 ];
